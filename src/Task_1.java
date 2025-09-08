f0edeae7bf81fc9d8a842102bb03f7a21cb9684c
Moohaha62 <miller.616732@swl.k12.oh.us>
2025-09-08 08:55:34 -0400

    Wowzers

--- src/Task_1.java
+++ src/Task_1.java
@@ -2,6 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Task_1 {
     public static void main(String[] args) {
-        System.out.println("Wowzers this is a test");
+        int intOperandA = 12;
+        int intOperandB = 8;
+        int intSum = intOperandA + intOperandB;;
+        int intDifference = intOperandA - intOperandB;
+        int intProduct = intOperandA * intOperandB;
+        int intQuotient = intOperandA / intOperandA;
+        int intModulo;
+        double doubleOperandA = 24;
+        double doubleOperandB = 16;
+        double doubleSum;
+        double doubleDifference;
+        double doubleProduct;
+        double doubleQuotient;
+        double doubleModulo;
+        System.out.println("The Sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        System.out.println("The Difference of " + intOperandA + " and " + intOperandB + " is " +intDifference);
+        System.out.println("The Product of " + intOperandA + " and " + intOperandB + " is " +intProduct);
+        System.out.println("The Quotient of " + intOperandA + " and " + intOperandB + " is " +intQuotient);
     }
 }
