ff0b77a70189e4fa6e1327c97d691b28773d28fd
Moohaha62 <miller.616732@swl.k12.oh.us>
2025-09-09 08:20:04 -0400

    9/9/2025 Task 1 8:19

--- src/Task_1.java
+++ src/Task_1.java
@@ -2,23 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Task_1 {
     public static void main(String[] args) {
-        int intOperandA = 12;
+        int intOperandA = 14;
         int intOperandB = 8;
         int intSum = intOperandA + intOperandB;;
         int intDifference = intOperandA - intOperandB;
         int intProduct = intOperandA * intOperandB;
         int intQuotient = intOperandA / intOperandA;
-        int intModulo;
-        double doubleOperandA = 24;
-        double doubleOperandB = 16;
+        int intModulo = 12 % 8;
+        double doubleOperandA = 14;
+        double doubleOperandB = 8;
         double doubleSum;
         double doubleDifference;
         double doubleProduct;
-        double doubleQuotient;
+        double doubleQuotient = doubleOperandA / doubleOperandB;
         double doubleModulo;
         System.out.println("The Sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
         System.out.println("The Difference of " + intOperandA + " and " + intOperandB + " is " +intDifference);
         System.out.println("The Product of " + intOperandA + " and " + intOperandB + " is " +intProduct);
-        System.out.println("The Quotient of " + intOperandA + " and " + intOperandB + " is " +intQuotient);
+        System.out.println("The Quotient of " + intOperandA + " and " + intOperandB + " is " +doubleQuotient);
+        System.out.println("The Modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo);
     }
 }
