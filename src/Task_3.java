3a9107ace84fc2389a5f71d2eddc8e8d6f858504
616732 <miller.616732@swl.k12.oh.us>
2025-09-10 08:20:00 -0400

    9/10/2025 Task 3 8:21

--- src/Task_3.java
+++ src/Task_3.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 public class Task_3 {
     public static void main(String[] args) {
     Scanner scan = new Scanner (System.in);
-
+    int fallCost;
+    int winterCost;
+    int springCost;
+    int summerCost;
+    int totalPrice;
+    System.out.println("Please enter your maintenance costs for the summer.");
+    summerCost = scan.nextInt();
+    System.out.println("PlLease enter your maintenance costs for the fall");
+    fallCost = scan.nextInt();
+    System.out.println("Please enter your maintenance costs for the winter");
+    winterCost = scan.nextInt();
+    System.out.println("Please enter your maintenance costs for the spring");
+    springCost = scan.nextInt();
+    totalPrice = fallCost + springCost + summerCost + winterCost;
+    System.out.println("Your total cost is $" + totalPrice);
     }
 }
